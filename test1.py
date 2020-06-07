9443cf3421751761c3b0274a11242d3dbb13eea4
h <ha.eshed@gmail.com>
2020-06-07 16:44:47 +0300

    commit 1

--- test1.py
+++ test1.py
@@ -1,51 +1,66 @@
 import cv2
 import datetime
 from vidstab.VidStab import VidStab
-from vidstab import layer_overlay, layer_blend
 import matplotlib.pyplot as plt
 import copy
 
+# initialize stabilizer
 stabilizer = VidStab()
-cap = cv2.VideoCapture('vtest2.avi')
-# cap = cv2.VideoCapture(0)
 
+# sets video capture source, live feed or using existing file
+cap = cv2.VideoCapture(0)
+# cap = cv2.VideoCapture('vtest3.avi')
+
+# set codec & font, initialize frame counter
 fourcc = cv2.VideoWriter_fourcc(*'XVID')
 font = cv2.FONT_HERSHEY_DUPLEX
 idframe = 0
 
-spar = 1
+# allows user to set parameter for size of frame & initializes new frame size
+spar = .5
 w = cap.get(cv2.CAP_PROP_FRAME_WIDTH) * spar
 h = cap.get(cv2.CAP_PROP_FRAME_HEIGHT) * spar
 while cap.isOpened():
     ret, frame = cap.read()
     idframe = idframe + 1
+    # check if frame valid
     if ret:
+        # defines text over raw frame & resize frame w/user set parameter
         text = 'Width: ' + str(w) + ' Height: ' + str(h) + ' frameid=' + str(idframe)
         datet = str(datetime.datetime.now())
         frame = cv2.resize(frame, None, fx=spar, fy=spar, interpolation=cv2.INTER_CUBIC)
-
+        # duplicates frame to avoid double text on stabilized frame
         frame2 = copy.copy(frame)
         frame2 = cv2.putText(frame2, text, (10, int(h-20)), font, .5, (255, 255, 255), 1, cv2.LINE_AA)
         frame2 = cv2.putText(frame2, datet, (10, 20), font, .5, (255, 255, 255), 1, cv2.LINE_AA)
 
+        # initiates stabilization parameter for first few frames, enables smooth stabilization from start
         window = idframe - 1 if idframe < 31 else 30
+        # stabilization function based on parameters given forehand
         res = stabilizer.stabilize_frame(input_frame=frame, smoothing_window=window, border_type='black', border_size=20)
+        # text addition (date, time, frame number) for stabilized frame
         text = 'Width: ' + str(w) + ' Height: ' + str(h) + ' frameid=' + str(idframe-1)
-        res = cv2.putText(res, text, (10, int(h-5)), font, .5, (255, 255, 255), 1, cv2.LINE_AA)
+        res = cv2.putText(res, text, (10, int(h+5)), font, .5, (255, 255, 255), 1, cv2.LINE_AA)
         res = cv2.putText(res, datet, (10, 30), font, .5, (255, 255, 255), 1, cv2.LINE_AA)
+        # res = res[int(h/8):int(h), int(w/8):int(w)]
 
+        # opens window containing frames (videos) of raw & stabilized side by side
         cv2.imshow('frame', frame2)
         cv2.imshow('stab_frame', res)
 
+    # waits for optional 'q' or 'esc' user keystroke to quit at any time
     keyboard = cv2.waitKey(1)
-    if keyboard == ord('q'):
-        break
+    if keyboard == ord('q') or keyboard == 27: break
+    # option to pause video using spacebar, continue on any key
+    if keyboard == 32: cv2.waitKey(0)
+    # auto-exits when video is over
+    if idframe == int(cap.get(cv2.CAP_PROP_FRAME_COUNT)): break
 
-
+# shows graphic representation of video stabilization trajectory, option for exporting data
 stabilizer.plot_trajectory()
 plt.show()
 stabilizer.plot_transforms()
 plt.show()
-
+# auto-release video capture & deletes open windows
 cap.release()
 cv2.destroyAllWindows()
